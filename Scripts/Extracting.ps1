6cc1c07bd1c6dde92c053c0926c40321534eb2d0
Prasanth <prasanth123n@gmail.com>
2018-07-17 13:17:30 +0530

    Msft-RDmiSaaSOffering fifth commit

--- Scripts/Extracting.ps1
+++ Scripts/Extracting.ps1
@@ -9,9 +9,16 @@
     
       
 )
-
+try
+{
 
 
 Invoke-WebRequest -Uri $fileURI -OutFile "C:\msft-rdmi-saas-offering.zip"
 New-Item -Path "C:\msft-rdmi-saas-offering" -ItemType directory -Force -ErrorAction SilentlyContinue
 Expand-Archive "C:\msft-rdmi-saas-offering.zip" -DestinationPath "C:\msft-rdmi-saas-offering" -ErrorAction SilentlyContinue
+
+}
+catch [Exception]
+{
+    Write-Output $_.Exception.Message
+}
