a8ddd7c9ef870ac31471d36538ef16a9fcbf5328
Prasanth <prasanth123n@gmail.com>
2018-07-17 13:05:36 +0530

    extracting.ps1 commit

---
+++ Scripts/Extracting.ps1
@@ -0,0 +1,17 @@
+﻿Param(
+
+    
+
+    [Parameter(Mandatory=$True)]
+    [ValidateNotNullOrEmpty()]
+    [string] $fileURI
+
+    
+      
+)
+
+
+
+Invoke-WebRequest -Uri $fileURI -OutFile "C:\msft-rdmi-saas-offering.zip"
+New-Item -Path "C:\msft-rdmi-saas-offering" -ItemType directory -Force -ErrorAction SilentlyContinue
+Expand-Archive "C:\msft-rdmi-saas-offering.zip" -DestinationPath "C:\msft-rdmi-saas-offering" -ErrorAction SilentlyContinue
